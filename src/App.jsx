deb282d7be63c943b46e139544adfc068a00fcc9
musicalmaya12 <109922285+musicalmaya12@users.noreply.github.com>
2026-02-18 07:08:28 +0000

    feat: add heart-hands emoji to SquareCares header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function App() {
-  return (
-    <div className="app">
-      <h1>SquareUp</h1>
-    </div>
-  )
+    return (
+        <div className="app">
+            <h1>SquareCares 🫶</h1>
+            <p>We know running a small business is a lot of hard work - and a lot of mental toll. We're here to help.</p>
+        </div>
+    )
 }
